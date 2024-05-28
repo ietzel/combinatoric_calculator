4fba81dd5361256726a0b489f94cd8fd27bb05b6
ietzel <67884277+ietzel@users.noreply.github.com>
2024-05-27 20:39:29 -0700

    Update comb_calc.pl

--- comb_calc.pl
+++ comb_calc.pl
@@ -1,36 +1,33 @@
-import math
-from tkinter import *
-
-def permutation(n, k) {
+sub permutation(n, k) {
   return (math.factorial(n)/math.factorial(n-k)) 
 }
 
-def combinatoric(n, k) {
+sub combinatoric(n, k) {
   return permutation(n, k)/math.factorial(k)))  
 }
 
-def stirling_second_kind(n, k) {
+sub stirling_second_kind(n, k) {
   sum = 0;
   for x in range(0, k):
     sum += (((-1)^(k-x))*combinatoric(n, x)*(x^n))
   return math.factorial(k)*sum
 }
 
-def permutation_sum(n, k) {
+sub permutation_sum(n, k) {
   sum = 0;
   for x in range(1, k):
     sum += permutation(n, x)
   return sum
 }
 
-def stirling_second_kind_sum(n, k) {
+sub stirling_second_kind_sum(n, k) {
   sum = 0;
   for x in range(1, k):
     sum += stirling_second_kind(n, k)
   return sum
 }
 
-def balls_and_boxes_problem(balls, boxes, dist_bls, dist_bxs, empty_allowed) {
+sub balls_and_boxes_problem(balls, boxes, dist_bls, dist_bxs, empty_allowed) {
   if(dist_bls) {
     return stirling_second_kind(n, k)
   } else if(dist_bxs) {
@@ -50,14 +47,7 @@ def balls_and_boxes_problem(balls, boxes, dist_bls, dist_bxs, empty_allowed) {
   }
 }
 
-root = Tk()
-w = Label(root, text='Combinatoric Calculator')
-w.pack()
-inputtext = tk.Text(root, height = 4, width = 8) 
-inputtext.pack() 
-root.mainloop()
-
-def main():
+sub main():
   items = input("Number of items? ")
   items = int(items)
   containers = input("Number of containers? ")
